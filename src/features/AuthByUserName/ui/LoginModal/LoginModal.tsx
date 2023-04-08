1a1072086ad2574a68e3e83498e858cb5bd8f0c1
a.nagikh <a.nagikh@MAC889.local>
2023-04-08 21:21:50 +0300

    lint css

--- src/features/AuthByUserName/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUserName/ui/LoginModal/LoginModal.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import LoginForm from '../LoginForm/LoginForm';
-import cls from './LoginModal.module.scss';
 
 interface LoginModalProps {
     className?: string;
