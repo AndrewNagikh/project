c4c5c5f8bc1243d1b5c47f0cdbb2269c31a70079
a.nagikh <a.nagikh@MAC889.local>
2023-04-14 22:49:58 +0300

    login slice test

--- src/features/AuthByUserName/model/slice/loginSlice.test.ts
+++ src/features/AuthByUserName/model/slice/loginSlice.test.ts
@@ -6,11 +6,11 @@ describe('login slice test', () => {
     test('test set username', () => {
         const state: DeepPartial<LoginSchema> = { username: '123' };
         expect(loginReducer(state as LoginSchema, loginActions.setLogin('123')))
-            .toBe({ username: '123' });
+            .toEqual({ username: '123' });
     });
     test('test set password', () => {
         const state: DeepPartial<LoginSchema> = { password: '123' };
         expect(loginReducer(state as LoginSchema, loginActions.setPassword('123')))
-            .toBe({ password: '123' });
+            .toEqual({ password: '123' });
     });
 });
